d976ecd5d08253d1e964f18710e951659a31ba41
saket940 <asdadarya2222@gmail.com>
2025-08-20 20:50:56 +0200

    Update main.jsx

--- ec/src/main.jsx
+++ ec/src/main.jsx
@@ -3,12 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import './App.css'
-import ErrorBoundary from "./ErrorBoundary";
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ErrorBoundary>
       <App />
-    </ErrorBoundary>
   </StrictMode>,
 )
